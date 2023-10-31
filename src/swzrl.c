7ed93dbe2299a1ee82cf7343583e41ede803536c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-10-31 10:29:16 +0100

    Implement swzrl_clear_screen()

--- src/swzrl.c
+++ src/swzrl.c
@@ -252,9 +252,11 @@ failed:
     return 80;
 }
 
-// -*-
+// -*- Clear the screen. Used to handle CTRL-L
 void swzrl_clear_screen(void){
-    //! @todo
+    if(write(STDOUT_FILENO, "\x1b[H\x1b[2J", 7) <= 0){
+        /* nothing to do, just to avoid warning. */
+    }
 }
 
 // -*-
