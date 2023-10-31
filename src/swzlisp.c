5c290b0f55961f5c02368e35ca976aa69a6d86a2
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-10-31 16:51:48 +0100

    Implement _swz_repl_parse_single_input()

--- src/swzlisp.c
+++ src/swzlisp.c
@@ -9,16 +9,67 @@
 
 #include "swzlisp.h"
 
-int disableSymcache = 0;
-int disbaleStrcache = 0;
-int lineContinue = 0;
+static int _disableSymcache = 0;
+static int _disbaleStrcache = 0;
+static int _lineContinue = 0;
 
 extern char **environ;
 
+
+// -*-
+void* _my_alloc(size_t size){
+    void *result = malloc(size);
+    if(result == NULL){
+        fprintf(stderr, "Error: memory allocation failure\n");
+        abort();
+    }
+    memset(result, 0, size);
+    return result;
+}
+
 // -*-
 static SWZObject* _swz_repl_parse_single_input(SWZRuntime *swz, EditLine *eline, History *hist){
-    //! @todo
-    return NULL;
+    char *input = NULL;
+    char *line = NULL;
+    int inputlen = 0;
+    int linelen = 0;
+    SWZObject *self;
+    HistEvent event;
+
+    _lineContinue = 0;
+    for(;;){
+        line = (char *)el_gets(eline, &linelen);
+        if(linelen <= 0){ /* 0 => EOF, -1 => error */
+            if(input){
+                free(input);
+            }
+            return swz_error(swz, SWZE_EXIT, "");
+        }
+        if(!input){
+            /* first time, our input is just the line. */
+            input = _my_alloc(linelen + 1);
+            strncpy(input, line, linelen + 1);
+            inputlen = linelen;
+        }else{
+            input = realloc(input, inputlen + linelen + 1);
+            strncat(input, line, linelen);
+            inputlen += linelen;
+        }
+
+        self = swz_parse_progn(swz, input);
+        if(self){
+            // - complete input!
+            history(hist, &event, H_ENTER, input);
+            free(input);
+            return self;
+        }else if(swz_get_errno(swz) != SWZE_EOF){
+            // - syntax error
+            free(input);
+            return NULL;
+        }
+        // - otherwise, partial line (EOF not expected)
+        _lineContinue = 1;
+    }
 }
 
 // -*-
