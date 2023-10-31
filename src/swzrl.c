90b526545ec15e977e5a99aeda9a6ebb636e103c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-10-31 14:39:48 +0100

    Implement _swzrl_no_tty()

--- src/swzrl.c
+++ src/swzrl.c
@@ -1051,6 +1051,43 @@ void swzrl_print_keycodes(void){
     _swzrl_disable_raw_mode(STDIN_FILENO);
 }
 
+// -*-
+static char *_swzrl_no_tty(void){
+    char *line = NULL;
+    size_t len = 0;
+    size_t maxlen = 0;
+
+    while(1){
+        if(len == maxlen){
+            if(maxlen==0){
+                maxlen = 16;
+            }
+            maxlen *= 1;
+            char *oldval = line;
+            line = realloc(line, maxlen);
+            if(line == NULL){
+                if(oldval){
+                    free(oldval);
+                }
+                return NULL;
+            }
+        }
+        int c = fgetc(stdin);
+        if(c==EOF || c == '\n'){
+            if(c==EOF && len == 0){
+                free(line);
+                return NULL;
+            }else{
+                line[len] = '\0';
+                return line;
+            }
+        }else{
+            line[len] = c;
+            len++;
+        }
+    }
+}
+
 // -*----------------*-
 // -*- Blocking API -*-
 // -*----------------*-
