3310dbd3084348f249995db37b9e8e8f47fb1427
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-14 11:04:39 +0100

    Define Env class skeleton

--- src/swzlisp.hpp
+++ src/swzlisp.hpp
@@ -14,6 +14,23 @@
 #include<cmath>
 #include<map>
 
+#define SWZLISP_TYPES               \
+    SWZLISP_DEF(Unit, "unit")       \
+    SWZLISP_DEF(Atom, "atom")       \
+    SWZLISP_DEF(Quote, "quote")     \
+    SWZLISP_DEF(Integer, "integer") \
+    SWZLISP_DEF(Float, "float")     \
+    SWZLISP_DEF(String, "string")   \
+    SWZLISP_DEF(List, "list")       \
+    SWZLISP_DEF(Lambda, "lambda")   \
+    SWZLISP_DEF(Builtin, "function")
+
+#define SWZLISP_EXCEPTIONS                              \
+    SWZLISP_DEF(TypeError, "TypeError")                 \
+    SWZLISP_DEF(ValueError, "ValueError")               \
+    SWZLISP_DEF(SyntaxError, "SyntaxError")             \
+    SWZLISP_DEF(RuntimError, "RuntimeError")            \
+    SWZLISP_DEF(ZeroDivisionError, "ZeroDivisionError")
 
 // -*-------------------------------------------------------------------*-
 // -*- namespace::swzlisp                                              -*-
@@ -21,85 +38,56 @@
 namespace swzlisp{
 // -*-
 enum class Type{
-    Unit,
-    Atom,
-    Quote,
-    Integer,
-    Float,
-    String,
-    List,
-    Lambda,
-    Builtin,
+#define SWZLISP_DEF(type, desc)     type,
+    SWZLISP_TYPES
+#undef SWZLISP_DEF
+};
+
+static std::map<Type, std::string> swzlispTypes = {
+#define SWZLISP_DEF(sym, desc)     {Type::sym, desc},
+    SWZLISP_TYPES
+#undef SWZLISP_DEF
+};
+
+enum class ErrorKind{
+#define SWZLISP_DEF(Err, desc) Err,
+    SWZLISP_EXCEPTIONS
+#undef SWZLISP_DEF
+};
+
+static std::map<ErrorKind, std::string> swzlispExceptions = {
+#define SWZLISP_DEF(err, desc)  {ErrorKind::err, desc},
+    SWZLISP_EXCEPTIONS
+#undef SWZLISP_DEF
 };
 
 // -*-
+
+// to_string()
+// replace()
+// is_symbol() | is_valid_char()
+
+// -*-
+template<typename T>
+class Env{
+public:
+    Env();
+    Env(const Env&);
+    bool contains(const std::string& name) const;
+    T get(const std::string& name) const;
+    void put(std::string name, T value);
+
+    friend std::ostream& operator<<(std::ostream& out, const Env&);
+
+private:
+    std::map<std::string, T> m_bindings;
+    std::shared_ptr<Env> m_parent;
+};
+
 class Object;
-class Env;
+//class Env;
+typedef Object (*Fun)(std::vector<Object>, Env<Object>&);
 
-// -*-
-
-class Base{
-public:
-    Base()=default;
-    virtual ~Base() = default;
-
-    virtual std::string type_name() = 0;
-    virtual std::string to_string() = 0;
-    virtual std::string repr() = 0;
-};
-
-
-typedef Object (*Fun)(std::vector<Object>, Env&);
-
-// Unit()
-class Unit: public Base {
-public:
-    Unit(): Base() {}
-
-    std::string type_name() override{
-        return "unit";
-    }
-
-    std::string to_string() override {
-        return "()";
-    }
-
-    std::string repr() override{
-        return "()";
-    }
-};
-
-// -*-
-// Quote(Object)
-class Quote{};
-
-// Atom(std::string)
-class Atom{};
-
-// Integer(long)
-class Integer {};
-
-// Float(double)
-class Float{};
-
-// String(std::string)
-class String{};
-
-// List(std::vector<Object>)
-class List;
-
-// Lambda(std::vector<Object>, Object, const Env&)
-class Lambda;
-
-// Builtin(std::string, Fun)
-class Builtin;
-
-
-// -*-
-class Error;
-
-// -*-
-class Env;
 
 // -*-
 class Object{
@@ -110,12 +98,13 @@ public:
     Object(std::vector<Object>);                        // Type::List
     Object(const Object&);                              // Type::Quote
     Object(std::string, Type);                          // Type::Atom or Type::String
-    Object(std::vector<Object>, Object, const Env&);    // Type::Lambda
-    Object(std::string, std::shared_ptr<Builtin>);      // Type::Builtin
+    Object(std::vector<Object>, Object, const Env<Object>&);    // Type::Lambda
+    Object(std::string, Fun);      // Type::Builtin
 
 
 private:
 };
+
 
 
 // -*-------------------------------------------------------------------*-
