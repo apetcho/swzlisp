58b7f45c550164496d9155d8efc1c7660aa228b7
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-15 15:07:36 +0100

    Implement fun_not_equalp() function

--- src/swzlisp.cpp
+++ src/swzlisp.cpp
@@ -467,7 +467,24 @@ static Object fun_equalp(std::vector<Object> args, Env& env){
     return result;
 }
 
-static Object fun_not_equalp(std::vector<Object> args, Env& env);
+// -*-
+static Object fun_not_equalp(std::vector<Object> args, Env& env){
+    evaluate(args, env);
+
+    if(args.size() != 2){
+        Object self = Object();
+        std::string msg = "Invalid '!=' expression.";
+        auto error = Error(self, env, msg.c_str());
+        throw Error(error);
+    }
+    
+    auto x = args[0];
+    auto y = args[1];
+    Object result = Object(static_cast<long>(x != y));
+    return result;
+}
+
+
 static Object fun_greaterp(std::vector<Object> args, Env& env);
 static Object fun_lessp(std::vector<Object> args, Env& env);
 static Object fun_greater_equalp(std::vector<Object> args, Env& env);
